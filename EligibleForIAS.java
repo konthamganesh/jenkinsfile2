125bb7960ac2116015f0055d024d603208b212bd
konthamganesh <89973081+konthamganesh@users.noreply.github.com>
2021-09-03 14:41:20 +0530

    eligibleornot.java

---
+++ EligibleForIAS.java
@@ -0,0 +1,15 @@
+ class EligibleForIAS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=26;
+		if((n>=24) && (n<=35)) {
+			System.out.println("eligible");
+		}
+		else
+		{
+			System.out.println("not eligible");
+		}
+	}
+
+}
